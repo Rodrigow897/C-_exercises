1572aea435b6516f925c72c36804357385654399
Rodrigo <rwesley2000@gmail.com>
2025-10-07 22:58:42 -0300

    added Exe-Interface

---
+++ Exe-Interface/SalvarJson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_Interface
+{
+    public class SalvarJson : ArquivoBase, ISalvar
+    {
+        public void Salvar()
+        {
+            Console.WriteLine("Salvar arquivo json");
+        }
+        public override void Nome()
+        {
+            Console.WriteLine("Definir nome json");
+        }
+    }
+}
---
+++ Exe-Interface/SalvarXml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe_Interface;
+public class SalvarXml : ArquivoBase, ISalvar
+{
+    public void Salvar()
+    {
+        Console.WriteLine("Salvar arquivo em Xml");
+    }
+    public override void Nome()
+    {
+        Console.WriteLine("Definir nome xml");
+    }
+}
